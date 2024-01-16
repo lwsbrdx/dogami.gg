37134d7df3f80403aec1b2bbba4181abe54e737f
lwsbrdx <madatman972@gmail.com>
2024-01-16 22:12:48 +0100

    fix: responsive header

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -19,14 +19,14 @@
                 <a href="{{ route('home') }}">
                     <img src="{{ Vite::asset('resources/assets/logo/logo-white-transparent.svg') }}" class="w-64"/>
                 </a>
-                <div class="pt-4 flex flex-col items-center sm:flex-row sm:pt-0 gap-6 px-10 w-full">
-                    <a href="{{ route('home') }}" class="font-bold text-xl">
+                <div class="pt-4 flex flex-col items-center sm:flex-row sm:pt-0 gap-6 px-10 w-full sm:text-xl">
+                    <a href="{{ route('home') }}" class="font-bold">
                         🏠 Home
                     </a>
-                    <a href="{{ route('leaderboards') }}" class="font-bold text-xl">
+                    <a href="{{ route('leaderboards') }}" class="font-bold">
                         🏆 Leaderboards
                     </a>
-                    <a href="#" class="font-bold text-xl">
+                    <a href="#" class="font-bold">
                         🔀 Compare Dogamis 🚧
                     </a>
                 </div>
