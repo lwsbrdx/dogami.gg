9207e2f20c1e2beb4c573a17f19bb20d8a2744f8
lwsbrdx <madatman972@gmail.com>
2024-01-17 12:33:23 +0100

    fix: responsive all dogamis + dogami card

--- resources/views/components/dogami_card.blade.php
+++ resources/views/components/dogami_card.blade.php
@@ -1,5 +1,5 @@
 <a href="{{ route('dogamis.one', $dogami->nftId) }}" class="w-min">
-    <div class="bg-[#2d123b] p-3 flex flex-col border rounded-md border-[#23102d] w-32 h-56 sm:w-40 sm:h-64 transition-all shadow-none shadow-zinc-950 hover:shadow-2xl hover:shadow-zinc-950">
+    <div class="text-xs sm:text-sm bg-[#2d123b] p-3 flex flex-col border rounded-md border-[#23102d] w-32 h-56 sm:w-40 sm:h-64 transition-all shadow-none shadow-zinc-950 hover:shadow-2xl hover:shadow-zinc-950">
         <img class="rounded-t-md pb-2" src="{{ $dogami->image }}" alt="{{ $dogami->name }}">
         <p class="text-center">{{ $dogami->name }}</p>
         @if ($dogami->isPuppy)
--- resources/views/dogamis/all.blade.php
+++ resources/views/dogamis/all.blade.php
@@ -5,7 +5,7 @@
     @include('components.forms.dogamis_search')
 </div>
 
-<div class="mx-auto justify-items-center grid grid-cols-2 w-80 sm:grid-cols-3 sm:w-[500px] lg:grid-cols-4 lg:w-[680px] xl:grid-cols-6 xl:w-[1080px] gap-x-0 gap-y-4 sm:gap-4 max-w-full">
+<div class="mx-auto justify-items-center grid grid-cols-2 w-80 sm:grid-cols-3 sm:w-[500px] lg:grid-cols-4 lg:w-[680px] xl:grid-cols-6 xl:w-[1080px] gap-x-0 gap-y-2 sm:gap-4 max-w-full">
     @empty($dogamis)
         <div>No Dogamis in database</div>
     @else
