b318a67ded699130a2edbd60bd4c1aac1c59c8a0
lwsbrdx <madatman972@gmail.com>
2024-01-17 07:37:38 +0100

    feat: compare aussi les niveaux des chiens

--- resources/views/compare/show.blade.php
+++ resources/views/compare/show.blade.php
@@ -23,6 +23,29 @@
                     <p class="text-center mt-4">{{ $dogamis[0]->name }}</p>
                 </div>
 
+                @if ($dogamis[0]->isPuppy)
+                    <div class="flex flex-col w-fit mx-auto text-center">
+                        <p>ID : {{ $dogamis[0]->nftId }}</p>
+                        <div class="flex flex-row items-center gap-2">
+                            @isset($dogamis[1])
+                                <p class="{{ $dogamis[0]->level < $dogamis[1]->level ? 'text-red-500' : 'text-green-500' }}">
+                                    Level : {{ $dogamis[0]->level }}
+                                </p>
+                            @else
+                                <p>Level : {{ $dogamis[0]->level }}</p>
+                            @endisset
+
+                            @isset($dogamis[1])
+                                @if ($dogamis[0]->level < $dogamis[1]->level)
+                                    <div class="w-0 h-0 border border-b-0 border-l-8 border-l-transparent border-r-8 border-r-transparent border-t-8 border-t-red-500"></div>
+                                @else
+                                    <div class="w-0 h-0 border border-t-0 border-l-8 border-l-transparent border-r-8 border-r-transparent border-b-8 border-b-green-500"></div>
+                                @endif
+                            @endisset
+                        </div>
+                    </div>
+                @endif
+
                 @if (count($dogamis[0]->skills) > 0)
                     @include('components.dogami_table', [
                         'dogami' => $dogamis[0],
@@ -37,6 +60,23 @@
                     <img class="rounded-md" src="{{ $dogamis[1]->image }}" alt="{{ $dogamis[1]->name }}">
                     <p class="text-center mt-4">{{ $dogamis[1]->name }}</p>
                 </div>
+
+                @if ($dogamis[1]->isPuppy)
+                    <div class="flex flex-col w-fit mx-auto text-center">
+                        <p>ID : {{ $dogamis[1]->nftId }}</p>
+                        <div class="flex flex-row items-center gap-2">
+                            <p class="{{ $dogamis[1]->level < $dogamis[0]->level ? 'text-red-500' : 'text-green-500' }}">
+                                Level : {{ $dogamis[1]->level }}
+                            </p>
+
+                            @if ($dogamis[1]->level < $dogamis[0]->level)
+                                <div class="w-0 h-0 border border-b-0 border-l-8 border-l-transparent border-r-8 border-r-transparent border-t-8 border-t-red-500"></div>
+                            @else
+                                <div class="w-0 h-0 border border-t-0 border-l-8 border-l-transparent border-r-8 border-r-transparent border-b-8 border-b-green-500"></div>
+                            @endif
+                        </div>
+                    </div>
+                @endif
 
                 @if (count($dogamis[1]->skills) > 0)
                     @include('components.dogami_table', [
