b490de9735afe0a3433e13fd376368ac8a9da002
lwsbrdx <madatman972@gmail.com>
2024-01-19 17:30:12 +0100

    responsive: page chiens par niveau en mobile les cards n'étaient pas centrées

--- resources/views/leaderboards/byLevel.blade.php
+++ resources/views/leaderboards/byLevel.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     @include('components.skills_selection')
 
-    <div class="flex flex-row flex-wrap gap-x-4 gap-y-4 py-8">
+    <div class="flex flex-row flex-wrap justify-center gap-x-4 gap-y-4 py-8">
         @foreach ($dogamis as $dogami)
             @include('components.dogami_card', ["dogami" => $dogami])
         @endforeach
