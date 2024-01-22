1db83f0408f62ba429f2061ddb558ba8bbe36685
lwsbrdx <madatman972@gmail.com>
2024-01-22 13:20:49 +0100

    feat: ajout de qq infos sur la vue d'1 Dogami

--- resources/views/dogamis/one.blade.php
+++ resources/views/dogamis/one.blade.php
@@ -2,9 +2,15 @@
 
 @section('content')
     @if ($dogami !== null)
-        <div class="min-w-60 w-min mx-auto">
+        <div class="min-w-60 w-min mx-auto text-center">
             <img class="rounded-md" src="{{ $dogami->image }}" alt="{{ $dogami->name }}">
-            <p class="text-center mt-4">{{ $dogami->name }}</p>
+            <p class="mt-4 mb-0">{{ $dogami->rarity }}</p>
+            <p class="mt-1">{{ $dogami->name }}</p>
+            <p class="mt-1 mb-4">{{ $dogami->breed->name }}</p>
+            <a target="_blank" href="https://objkt.com/tokens/dogami/{{ $dogami->nftId }}" class="flex flex-row items-center justify-center">
+                <img class="inline" src="{{ Vite::asset('resources/assets/svg/link.svg') }}" alt="">
+                <span class="ml-2">See on objkt.com</span>
+            </a>
         </div>
 
         <div class="flex flex-col gap-2 w-fit mx-auto my-3">
