5e27a9d9befda60f76ae78874246c563ed94a7a9
lwsbrdx <madatman972@gmail.com>
2024-01-23 13:57:29 +0100

    update dogamis */4 hours to avoid bigger billing :) merci railway...

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('dogamis:update:all')->everyTwoHours();
+        $schedule->command('dogamis:update:all')->everyFourHours();
     }
 
     /**
