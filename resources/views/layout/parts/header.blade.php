3c6dab794d30eb8c8ef6baabc03bcb75d08b8487
lwsbrdx <madatman972@gmail.com>
2024-01-30 20:21:05 +0100

    feat: remove icons in header

--- resources/views/layout/parts/header.blade.php
+++ resources/views/layout/parts/header.blade.php
@@ -3,14 +3,8 @@
         <img src="{{ Vite::asset('resources/assets/logo/logo-white-transparent.svg') }}" class="w-64"/>
     </a>
     <div class="pt-4 flex flex-col items-center sm:flex-row sm:pt-0 gap-6 px-10 w-full sm:text-xl">
-        <a href="{{ route('home') }}" class="font-bold">
-            🏠 Home
-        </a>
-        <a href="{{ route('leaderboards') }}" class="font-bold">
-            🏆 Leaderboards
-        </a>
-        <a href="{{ route('compare') }}" class="font-bold">
-            🔀 Compare Dogamis
-        </a>
+        <a href="{{ route('home') }}" class="font-bold">Home</a>
+        <a href="{{ route('leaderboards') }}" class="font-bold">Leaderboards</a>
+        <a href="{{ route('compare') }}" class="font-bold">Compare Dogamis</a>
     </div>
 </header>
