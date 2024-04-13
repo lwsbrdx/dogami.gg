342185b3b37e61b8e9cfb9d81de4949519b41ace
lwsbrdx <madatman972@gmail.com>
2024-04-13 02:00:12 +0200

    feat: declares in consts rankings types

--- app/Models/DogamisRank.php
+++ app/Models/DogamisRank.php
@@ -10,6 +10,8 @@ use MongoDB\Laravel\Eloquent\Model;
  * Dogami Rank
  *
  * @property int $ranking
+ * @property string $ranking_type
+ * @property string $value_type
  * @property string $skill_type
  * @property int $skill_value
  * @property int[] $dogamis
@@ -18,6 +20,18 @@ class DogamisRank extends Model {
     public const ACTUAL_VALUE = 'actual';
     public const MAX_VALUE = 'max';
 
+    public const GLOBAL_RANKING = 'global';
+    public const BREED_RANKING = 'breed';
+    public const RARITY_RANKING = 'rarity';
+    public const COLLECTION_RANKING = 'collection';
+
+    public const RANKING_TYPES = [
+         self::GLOBAL_RANKING,
+         self::BREED_RANKING,
+         self::RARITY_RANKING,
+         self::COLLECTION_RANKING,
+    ];
+    
     protected $primaryKey = 'ranking';
     protected $keyType = 'int';
     public $incrementing = false;
