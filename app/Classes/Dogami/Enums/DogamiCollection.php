daac6744149d5d40f98a3fec7f2428356b99366e
lwsbrdx <madatman972@gmail.com>
2024-04-15 15:42:32 +0200

    fix: fix naming collections

--- app/Classes/Dogami/Enums/DogamiCollection.php
+++ app/Classes/Dogami/Enums/DogamiCollection.php
@@ -4,7 +4,7 @@ namespace App\Classes\Dogami\Enums;
 
 enum DogamiCollection: string
 {
-    case AlphaS1 = "Alpha Series 1";
-    case AlphaS2 = "Alpha Series 2";
-    case GammaS1 = "Gamma Series 1";
+    case AlphaS1 = "Alpha - Series I";
+    case AlphaS2 = "Alpha - Series II";
+    case GammaS1 = "Gamma - Series I";
 }
